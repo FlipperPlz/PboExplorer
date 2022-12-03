4ca2da26b0e726cbd99f82c0a7dea4127394f1ce
Ryann (Flipper) <flipperplz@protonmail.com>
2022-12-02 18:03:56 -0800

    feat(core): Add Base Views

---
+++ PboExplorer/Windows/SplashWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PboExplorer.Windows
+{
+    /// <summary>
+    /// Interaction logic for SplashWindow.xaml
+    /// </summary>
+    public partial class SplashWindow : Window
+    {
+        public SplashWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenPBOFile(object sender, RoutedEventArgs e)
+        {
+            
+            Close();
+        }
+
+        private void CreateNewPBO(object sender, RoutedEventArgs e)
+        {
+
+            Close();
+        }
+
+       
+    }
+}
