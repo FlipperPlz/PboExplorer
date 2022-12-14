9c21116225c3a44a6718d416d8ab9f26f801f843
Ryann (Flipper) <flipperplz@protonmail.com>
2022-12-14 11:08:45 -0800

    chore(base): Combine EntryDataRepository, Stream and Manager into one file

--- PboExplorer/TreeItems/TreeDirectoryEntry.cs
+++ PboExplorer/TreeItems/TreeDirectoryEntry.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using BisUtils.PBO.Entries;
 using PboExplorer.Utils;
 
 namespace PboExplorer.TreeItems; 
