76b40c7035c795f83a1c4cb8da18ed3a6a75f66e
rvost <romanvost@gmail.com>
2022-12-15 23:20:57 +0300

    feat(ui): Add Most Recently Used files list to SplashWindow
    - Add persisted list of MRU files to SplashWindow;
    - Refactor SplashWindow code-behind;
    - Introduce view model for SplashWindow;

--- PboExplorer/Windows/SplashWindow.xaml.cs
+++ PboExplorer/Windows/SplashWindow.xaml.cs
@@ -1,20 +1,6 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using BisUtils.PBO;
-using Path = System.IO.Path;
+using PboExplorer.ViewModels;
 
 namespace PboExplorer.Windows
 {
@@ -23,35 +9,20 @@ namespace PboExplorer.Windows
     /// </summary>
     public partial class SplashWindow : Window
     {
+        private SplashViewModel _viewModel;
+
         public SplashWindow()
         {
             InitializeComponent();
+            _viewModel = new();
+            _viewModel.CloseRequested += OnCloseRequested;
+            DataContext = _viewModel;
         }
 
-        private void OpenPBOFile(object sender, RoutedEventArgs e)
+        private void OnCloseRequested(object? sender, EventArgs e)
         {
-            var dlg = new OpenFileDialog();
-            dlg.Title = "Load PBO archive";
-            dlg.DefaultExt = ".pbo";
-            dlg.Filter = "PBO File|*.pbo|Preview BI Files|*.paa;*.rvmat;*.bin;*.pac;*.p3d;*.wrp;*.sqm";
-            if (dlg.ShowDialog() != true) return;
-            
-            new PboExplorerWindow(new PboFile(dlg.FileName)).Show();
-            Close();
-
-        }
-
-        private void CreateNewPBO(object sender, RoutedEventArgs e)
-        {
-            var dlg = new OpenFileDialog();
-            dlg.Title = "Load PBO archive";
-            dlg.DefaultExt = ".pbo";
-            dlg.Filter = "PBO File|*.pbo|Preview BI Files|*.paa;*.rvmat;*.bin;*.pac;*.p3d;*.wrp;*.sqm";
-            if (dlg.ShowDialog() != true) return;
-            new PboExplorerWindow(new PboFile(dlg.FileName, PboFileOption.Create)).Show();
+            _viewModel.CloseRequested -= OnCloseRequested;
             Close();
         }
-
-       
     }
 }
